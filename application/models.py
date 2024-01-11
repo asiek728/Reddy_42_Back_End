2eb5d6486a12c3ad3bfe35529f7a494f40e7b8dd
Esham <Esham156@gmail.com>
2024-01-11 15:54:25 +0000

    add messages folder to improve architecture

--- application/models.py
+++ application/models.py
@@ -74,21 +74,7 @@
 
 
 #chat
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    room = db.Column(db.String(50), nullable=False)
-    author = db.Column(db.String(50), nullable=False)
-    content = db.Column(db.String(500), nullable=False)
-    time = db.Column(db.String(20), nullable=False)
 
-    def __init__(self, room, author, content, time):
-        self.room = room
-        self.author = author
-        self.content = content
-        self.time = time
-
-    def __repr__(self):
-        return f"Message('{self.room}', '{self.author}', '{self.content}', '{self.time}')"
 
 # class GP(db.Model):
 #     pass
